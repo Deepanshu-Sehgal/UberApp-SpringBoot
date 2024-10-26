a2e12fc20782057615e94da7257dc8c31fd2e0de
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2024-10-26 22:29:05 +0530

    init

--- src/main/java/com/datricle/project/uber/UberApp/strategies/impl/DriverMatchingHighestDriverStrategy.java
+++ src/main/java/com/datricle/project/uber/UberApp/strategies/impl/DriverMatchingHighestDriverStrategy.java
@@ -3,12 +3,10 @@ package com.datricle.project.uber.UberApp.strategies.impl;
 import com.datricle.project.uber.UberApp.dto.RideRequestDto;
 import com.datricle.project.uber.UberApp.entities.Driver;
 import com.datricle.project.uber.UberApp.strategies.DriverMatchingStrategy;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class DriverMatchingNearestStrategyImpl implements DriverMatchingStrategy {
+public class DriverMatchingHighestDriverStrategy implements DriverMatchingStrategy {
     @Override
     public List<Driver> findMatchingDriver(RideRequestDto rideRequestDto) {
         return List.of();
--- src/main/java/com/datricle/project/uber/UberApp/strategies/impl/RideFareCalculationStrategyImpl.java
+++ src/main/java/com/datricle/project/uber/UberApp/strategies/impl/RideFareCalculationStrategyImpl.java
@@ -2,7 +2,9 @@ package com.datricle.project.uber.UberApp.strategies.impl;
 
 import com.datricle.project.uber.UberApp.dto.RideRequestDto;
 import com.datricle.project.uber.UberApp.strategies.RideFareCalculationStrategy;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RideFareCalculationStrategyImpl implements RideFareCalculationStrategy
 {
     @Override
