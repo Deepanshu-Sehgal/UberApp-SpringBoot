b2c42749333d2f6efdc13ff76018f01ede906295
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2024-10-26 19:30:19 +0530

    init

--- src/main/java/com/datricle/project/uber/UberApp/repositories/DriverRepository.java
+++ src/main/java/com/datricle/project/uber/UberApp/repositories/DriverRepository.java
@@ -2,7 +2,9 @@ package com.datricle.project.uber.UberApp.repositories;
 
 import com.datricle.project.uber.UberApp.entities.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver,Long> {
 
 }
--- src/main/java/com/datricle/project/uber/UberApp/repositories/RideRepository.java
+++ src/main/java/com/datricle/project/uber/UberApp/repositories/RideRepository.java
@@ -2,6 +2,8 @@ package com.datricle.project.uber.UberApp.repositories;
 
 import com.datricle.project.uber.UberApp.entities.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RideRepository extends JpaRepository<Ride,Long> {
 }
