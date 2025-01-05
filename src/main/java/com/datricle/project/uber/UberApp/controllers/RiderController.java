b53245d8e602c12eb51a48b771e20473e88edc06
Deepanshu Sehgal <deepanshusehgal.112@gmail.com>
2025-01-05 13:18:01 +0530

    init

--- src/main/java/com/datricle/project/uber/UberApp/controllers/RiderController.java
+++ src/main/java/com/datricle/project/uber/UberApp/controllers/RiderController.java
@@ -1,13 +1,11 @@
 package com.datricle.project.uber.UberApp.controllers;
 
+import com.datricle.project.uber.UberApp.dto.RideDto;
 import com.datricle.project.uber.UberApp.dto.RideRequestDto;
 import com.datricle.project.uber.UberApp.services.RiderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/riders")
@@ -19,5 +17,10 @@ public class RiderController {
     @PostMapping("/requestRide")
     public ResponseEntity<RideRequestDto> requestRide(@RequestBody RideRequestDto rideRequestDto) {
         return ResponseEntity.ok(riderService.requestRide(rideRequestDto));
+    }
+
+    @PostMapping("/cancelRide/{rideId}")
+    public ResponseEntity<RideDto> cancelRide(@PathVariable Long rideId){
+        return ResponseEntity.ok(riderService.cancelRide(rideId));
     }
 }
